b77b697744f5f90248bf30dcdcf432e5ace267b3
김채연 <chayeon0303@naver.com>
2025-08-26 08:40:36 +0900

    🌱baekjoon_10828

---
+++ src/stack_queue/baekjoon_10828/Main.java
@@ -0,0 +1,35 @@
+package stack_queue.baekjoon_10828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String m = st.nextToken();
+            
+            if(m.equals("push")){
+                int c = Integer.parseInt(st.nextToken());
+                stack.push(c);
+            } else if (m.equals("pop")) {
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+            } else if (m.equals("size")) {
+                sb.append(stack.size()).append("\n");
+            } else if (m.equals("empty")) {
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+            } else if (m.equals("top")) {
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
