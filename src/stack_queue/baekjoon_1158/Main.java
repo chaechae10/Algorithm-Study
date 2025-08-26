eea244c444a970a2c1e50c3e9858ba3d66fe6d5f
김채연 <chayeon0303@naver.com>
2025-08-27 08:28:37 +0900

    🌱[Stack & Queue]baekjoon_1158

---
+++ src/stack_queue/baekjoon_1158/Main.java
@@ -0,0 +1,38 @@
+package stack_queue.baekjoon_1158;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for(int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while (!queue.isEmpty()){
+            for(int i=0; i < k-1; i++){
+                queue.add(queue.poll());
+            }
+            sb.append(queue.poll());
+            if(!queue.isEmpty()){
+                sb.append(", ");
+            }
+        }
+        sb.append(">");
+        System.out.println(sb);
+    }
+}
