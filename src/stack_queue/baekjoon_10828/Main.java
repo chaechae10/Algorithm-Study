7d6463af4aeefe62151b39ba9c5d60ba0a2c1a0b
김채연 <chayeon0303@naver.com>
2025-08-26 09:09:51 +0900

    🌱[Fix] baekjoon_10828

--- src/stack_queue/baekjoon_10828/Main.java
+++ src/stack_queue/baekjoon_10828/Main.java
@@ -3,14 +3,16 @@ package stack_queue.baekjoon_10828;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+
+        Deque<Integer> stack = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i < N; i++){
